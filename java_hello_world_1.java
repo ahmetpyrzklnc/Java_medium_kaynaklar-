4a8f290dfd4c2e2c25656673b4695c8464565b28
Ahmet Kılınç <95951871+ahmetpyrzklnc@users.noreply.github.com>
2022-04-09 23:18:04 +0300

    Add files via upload

---
+++ java_hello_world_1.java
@@ -0,0 +1,11 @@
+package java_medium_blogu;
+
+public class java_hello_world_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ java_hello_world_2.java
@@ -0,0 +1,14 @@
+package java_medium_blogu;
+
+public class java_hello_world_2 {
+
+	public static void main(String[] args) {
+		
+		System.
+		      out.
+		          println
+		                ("Hello World!");
+
+	}
+
+}
